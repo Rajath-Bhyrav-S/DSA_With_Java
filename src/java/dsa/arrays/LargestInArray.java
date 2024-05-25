d38a12aaf30f7491d55d08ecc2cf5ce71e622819
Rajath-Bhyrav-S <rajathbhyrav147@outlook.com>
2024-05-25 13:40:12 +0530

    Not working in the laptop

---
+++ src/java/dsa/arrays/LargestInArray.java
@@ -0,0 +1,27 @@
+package java.dsa.arrays;
+import java.util.*;
+
+
+
+public class LargestInArray 
+{
+	public static int FindTheLargest(int Number[])
+	{
+		int largest = Integer.MIN_VALUE;
+		for(int i=0;i<Number.length;i++)
+		{
+			if(largest<Number[i])
+			{
+				largest=Number[i];
+			}
+		}
+		return largest;
+	}
+	public static void main(String[] args) 
+	{
+		int Numbers[]= {1,5,7,6,8};
+		System.out.println("The largest number in the array is" + FindTheLargest(Numbers));
+		
+	}
+
+}
